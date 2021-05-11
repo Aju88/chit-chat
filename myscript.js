609fe5f175541eff3d4075095c57284499f84581
Arjun Nair <arjun@vinnovatelabz.com>
2021-05-11 17:52:52 +0530

    updated code

--- myscript.js
+++ myscript.js
@@ -9,6 +9,8 @@ let userName = null,
       // User is signed in.
       console.log("logged in as " + user.displayName);
       userName = user.displayName;
+      // console.log(userName);
+
       uid = user.uid;
     } else {
       uid = null;
@@ -27,47 +29,78 @@ let userName = null,
 //---------------------set the receiver--------------------//
 function selectUser(user) {
   receiver = user;
+  document.getElementById("messages-received").innerHTML = "";
+
+  getData();
 }
 
 var db = firebase.database();
+db.ref("Messages/" + userName).set({
+  message: "logged in as " + userName,
+});
 
 //--------------------send a message---------------------//
+
 function saveMessage() {
   var message = document.getElementById("message-sent").value;
-  db.ref("Messages/" + receiver + "/" + userName)
-  .set({
+  db.ref("Messages/" + receiver + "/" + userName).set({
     sender: userName,
     message: message,
   });
-
-  getData();
+  getSentData();
   return false;
 }
 
-//-------------get message from database---------------//
+//-------------get message received-------------------//
+
 function getData() {
   console.log(userName);
 
-  var user_ref = db.ref("Messages/" + receiver + "/" + userName);
+  var user_ref = db.ref("Messages/" + userName + "/" + receiver);
   user_ref.on("value", (snapshot) => {
     var data = snapshot.val();
     console.log(data);
 
     var html = "";
     html += "<li>";
-    html += userName + " : " + data.message;
+    html += data.sender + " : " + data.message;
     html += "</li>";
     document.getElementById("messages-received").innerHTML += html;
   });
 }
 
-//--------------------getting the availabel contacts to send messsage-------------------//
+//---------------to get sent meessage ------------------------//
+function getSentData() {
+  console.log(userName);
+  var html;
+  var user_ref = db.ref("Messages/" + receiver + "/" + userName);
+  user_ref.on("value", (snapshot) => {
+    var data = snapshot.val();
+    console.log(data);
+
+    html = "";
+    html += "<li>";
+    html += "You" + " : " + data.message;
+    html += "</li>";
+    document.getElementById("messages-received").innerHTML += html;
+  });
+}
+
+//--------------------getting the available contacts to send messsage-------------------//
 
 function getContacts() {
+  var temp = "";
   var user_ref = db.ref("Messages");
   user_ref.on("value", function (snapshot) {
     var users = Object.getOwnPropertyNames(snapshot.val());
-    console.log("users are: " + users);
+    // console.log("users are: " + users);
+    for (let j = 0; j < users.length; j++) {
+      if (users[j] === userName) {
+        temp = users[0];
+        users[0] = users[j];
+        users[j] = temp;
+      }
+    }
     var html = "";
 
     for (let i = 0; i < users.length; i++) {
